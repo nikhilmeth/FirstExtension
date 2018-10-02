f2b8b1cb2c0a2b749b910609fe20c9b6b5bdd1ef
Nikhilesh Methuku <nikhil.methuku@gmail.com>
2018-10-02 18:56:55 -0500

    Added pop.js, popup.html and other files

---
+++ background.js
@@ -0,0 +1,14 @@
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.storage.sync.set({color: '#5f9ea0'}, function() {
+        console.log("The color is cadetblue.");
+    });
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        chrome.declarativeContent.onPageChanged.addRules([{
+            conditions: [new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: {hostEquals: 'developer.chrome.com'},
+            })
+            ],
+                actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
+    });
+});

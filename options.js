f7bb896aaaeffa987fa6adf69d73c713b47b0872
Nikhilesh Methuku <nikhil.methuku@gmail.com>
2018-10-02 19:21:25 -0500

    changed colors

---
+++ options.js
@@ -0,0 +1,15 @@
+let page = document.getElementById('buttonDiv');
+const kButtonColors = ['#DDDDDD', '#0074D9', '#001f3f', '#7FDBFF'];
+function constructOptions(kButtonColors) {
+  for (let item of kButtonColors) {
+    let button = document.createElement('button');
+    button.style.backgroundColor = item;
+    button.addEventListener('click', function() {
+      chrome.storage.sync.set({color: item}, function() {
+        console.log('color is ' + item);
+      })
+    });
+    page.appendChild(button);
+  }
+}
+constructOptions(kButtonColors);

236389903ed40c9bf51fe358f9cdc05ca47c7410
Nikhilesh Methuku <nikhil.methuku@gmail.com>
2018-10-02 19:31:32 -0500

    show date and time

--- popup.js
+++ popup.js
@@ -1,4 +1,14 @@
 let changeColor = document.getElementById('changeColor');
+let showDate = document.getElementById('showDate');
+let showTime = document.getElementById('showTime');
+// get a new date (locale machine date time)
+let date = new Date();
+// get the date as a string
+let n = date.toDateString();
+// get the time as a string
+let time = date.toLocaleTimeString();
+showDate.innerHTML= n;
+showTime.innerHTML= time;
 
 changeColor.onclick = function(element) {
     let color = element.target.value;
